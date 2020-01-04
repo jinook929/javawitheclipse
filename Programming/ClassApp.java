d6c73a467d538b28e6f19eb943db5a435a221921
jinook929 <jinook929@gmail.com>
2020-01-04 12:21:29 -0500

    Instance App

--- Programming/ClassApp.java
+++ Programming/ClassApp.java
@@ -1,7 +1,8 @@
+import java.io.FileNotFoundException;
 
 public class ClassApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		System.out.println(Math.PI);
 		System.out.println(Math.floor(1.6));

c730743672d317d033df0b8c78f645180880995d
jinook929 <jinook929@gmail.com>
2020-01-07 22:37:50 -0500

    If app

---
+++ Java-flow-control/AuthApp.java
@@ -0,0 +1,23 @@
+
+public class AuthApp {
+
+	public static void main(String[] args) {
+
+		String id = "jjo";
+		String inputId = args[0];
+		
+		System.out.println("Hi.");
+		
+//		if(inputId == id) {
+//			
+//		}
+		
+		if(inputId.equals(id)) {
+			System.out.println("Master!");
+		} else {
+			System.out.println("Who are you?");
+		}
+
+	}
+
+}

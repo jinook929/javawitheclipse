1bc62929627b4e2bd8bc17a24e38fe3cb6f872ff
jinook929 <jinook929@gmail.com>
2020-01-10 20:43:06 -0500

    Method

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,17 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+
+//		System.out.println("Hello Method");
+//		System.out.println(Math.floor(1.1));
+		
+		printExample();
+
+	}
+
+	public static void printExample() {
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1));
+	}
+}
---
+++ ProjectMethod/WhyMethod.java
@@ -0,0 +1,30 @@
+
+public class WhyMethod {
+
+	public static void main(String[] args) {
+
+		//...
+		printATwoTimes();
+//		System.out.println("-");
+//		System.out.println("B");
+//		System.out.println("B");
+		//...
+		printATwoTimes();
+//		System.out.println("-");
+//		System.out.println("B");
+//		System.out.println("B");
+		//...
+		printATwoTimes();
+//		System.out.println("-");
+//		System.out.println("B");
+//		System.out.println("B");
+
+	}
+	
+	public static void printATwoTimes() {
+		System.out.println("-");
+		System.out.println("A");
+		System.out.println("A");
+	}
+
+}

35930ce6e82ca22187c9c7eb5cde6088530dca70
jinook929 <jinook929@gmail.com>
2020-01-18 22:59:40 -0500

    separate class file

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -3,33 +3,32 @@ public class MyOOP {
 	
 	public static void main(String[] args) {
 
-		delimiter = "----";
-		printA();
-		printA();
-		printB();
-		printB();
+		Print.delimiter = "----";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
 		
-		delimiter = "****";
-		printA();
-		printA();
-		printB();
-		printB();
-		
-		
+		Print.delimiter = "****";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+				
 	}
 	
-	public static String delimiter = "";
-
-	public static void printA() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	}
-
-	public static void printB() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	}
+//	public static void printA() {
+//		System.out.println(delimiter);
+//		System.out.println("A");
+//		System.out.println("A");
+//	}
+//
+//	public static void printB() {
+//		System.out.println(delimiter);
+//		System.out.println("B");
+//		System.out.println("B");
+//	}
 	
 }
+
+

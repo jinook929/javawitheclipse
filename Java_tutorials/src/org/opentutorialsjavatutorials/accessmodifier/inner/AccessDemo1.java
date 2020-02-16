45190e2872c764a0533bb8e81c72ad4742ecb21e
jinook929 <jinook929@gmail.com>
2020-02-15 20:55:51 -0500

    access modifier

---
+++ Java_tutorials/src/org/opentutorialsjavatutorials/accessmodifier/inner/AccessDemo1.java
@@ -0,0 +1,25 @@
+package org.opentutorialsjavatutorials.accessmodifier.inner;
+
+class A {
+	public String x() {
+		return "public void x()";
+	}
+	
+	private String y() {
+		return "public void y()";
+	}
+	
+	public String z() {
+		return y();
+	}
+}
+
+public class AccessDemo1 {	
+	public static void main(String[] args) {
+		A a = new A();
+		
+		System.out.println(a.x());
+//		System.out.println(a.y()); // error
+		System.out.println(a.z());
+	}
+}

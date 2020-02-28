0436e97798319a9e7bcab7b995df4ddece26118f
jinook929 <jinook929@gmail.com>
2020-02-27 21:37:17 -0500

    throws exceptions

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
@@ -0,0 +1,36 @@
+package org.opentutorials.javatutorials.exception;
+
+import java.io.*;
+
+class B {
+	void run() throws FileNotFoundException, IOException {
+		BufferedReader bReader = null;
+		String input = null;
+		bReader = new BufferedReader(new FileReader("ut.txt"));
+		input = bReader.readLine();
+		System.out.println(input);
+	}
+}
+
+class C {
+	void run() throws FileNotFoundException, IOException {
+		B b = new B();
+		b.run();		
+	}
+}
+
+public class ThrowExceptionDemo {
+
+	public static void main(String[] args) {
+		C c = new C();
+		try {
+			c.run();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			System.out.println("There should be 'ut.txt' file.");
+			e.printStackTrace();
+		}
+
+	}
+
+}

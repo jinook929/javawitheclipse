9c1ec1fb830e361b24528af2780a2247200745fc
jinook929 <jinook929@gmail.com>
2020-03-01 22:09:59 -0500

    Exception

--- Java_tutorials/src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -3,10 +3,16 @@ package org.opentutorials.javatutorials.exception;
 class Calculator {
 	int firstNum, secondNum;
 	public void setOperands(int firstNum, int secondNum) {
+//		if(secondNum == 0) {
+//			throw new IllegalArgumentException("The secondNum cannot be zero.");
+//		}
 		this.firstNum = firstNum;
 		this.secondNum = secondNum;
 	}
 	public void divide() {
+		if(secondNum == 0) {
+			throw new ArithmeticException("The divisor cannot be zero.");
+		}
 		try {
 			System.out.print("계산 결과는 ");
 		System.out.print(this.firstNum / this.secondNum);
@@ -25,6 +31,10 @@ public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 = new Calculator();
 		c1.setOperands(10, 0);
-		c1.divide();
+		try {
+			c1.divide();
+		} catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }

38df4f0a5b31f7ae77f3fda795c759c29cfa1601
jinook929 <jinook929@gmail.com>
2020-03-02 21:54:24 -0500

    throw exception

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo2.java
@@ -0,0 +1,24 @@
+package org.opentutorials.javatutorials.exception;
+
+import java.io.IOException;
+
+class E {
+	void throwArithmeticException() {
+		throw new ArithmeticException();
+	}
+	void throwIOException1() throws IOException {
+		throw new IOException();
+	}
+	void throwIOException2() {
+		try {
+			throw new IOException();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+public class ThrowExceptionDemo2 {
+
+}

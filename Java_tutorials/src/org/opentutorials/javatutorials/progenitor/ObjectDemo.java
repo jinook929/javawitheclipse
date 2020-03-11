5c52b8bad41abe9b9c67018d21390bdf017b959d
jinook929 <jinook929@gmail.com>
2020-03-10 21:41:03 -0400

    clone()

--- Java_tutorials/src/org/opentutorials/javatutorials/progenitor/ObjectDemo.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/progenitor/ObjectDemo.java
@@ -1,9 +1,19 @@
 package org.opentutorials.javatutorials.progenitor;
 
-class Student {
+//class Human {
+//	protected String test() {
+//		return "test";
+//	}
+//}
+
+class Student implements Cloneable{
 	String name;
 	Student(String name) {
 		this.name = name;
+	}
+	
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 	
 	public boolean equals(Object obj) {
@@ -16,9 +26,19 @@ public class ObjectDemo {
 
 	public static void main(String[] args) {
 		Student s1 = new Student("jjo");
-		Student s2 = new Student("jjo");
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
+//		Student s2 = new Student("jjo");
+//		System.out.println(s1 == s2);
+//		System.out.println(s1.equals(s2));
+		try {
+			Student s2 = (Student) s1.clone();
+			System.out.println(s1.name);
+			System.out.println(s2.name);
+			System.out.println(s1 == s2);
+			System.out.println(s1.equals(s2));
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }

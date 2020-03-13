33e49c70af2b4ca64cbc309c7faf44d585246cc3
jinook929 <jinook929@gmail.com>
2020-03-12 22:47:11 -0400

    using interface

--- Java_tutorials/src/org/opentutorials/javatutorials/constant/ConstantDemo.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/constant/ConstantDemo.java
@@ -1,11 +1,23 @@
 package org.opentutorials.javatutorials.constant;
 
+interface FRUIT {
+	int APPLE = 1, PEACH = 2, BANANA = 3;
+}
+
+interface COMPANY {
+	int GOOGLE = 1, APPLE = 2, ORACLE = 3;
+}
+
 public class ConstantDemo {
 
-	private final static int APPLE = 1;
-	private final static int PEACH = 2;
-	private final static int BANANA = 3;
-	
+//	private final static int FRUIT_APPLE = 1;
+//	private final static int FRUIT_PEACH = 2;
+//	private final static int FRUIT_BANANA = 3;
+//	
+//	private final static int COMPANY_GOOGLE = 1;
+//	private final static int COMPANY_APPLE = 2;
+//	private final static int COMPANY_ORACLE = 3;
+//	
 	public static void main(String[] args) {
 		/*
 		 * 1. apple
@@ -25,15 +37,19 @@ public class ConstantDemo {
 				break;
 		}
 		
-		int type_ = APPLE;
+//		if(FRUIT.APPLE == COMPANY.APPLE) {
+//			System.out.println("Fruit Apple equals to Company Apple.");
+//		}
+		
+		int type_ = FRUIT.APPLE;
 		switch(type) {
-		case APPLE:
+		case FRUIT.APPLE:
 			System.out.println(57 + " kcal");
 			break;
-		case PEACH:
+		case FRUIT.PEACH:
 			System.out.println(34 + " kcal");
 			break;
-		case BANANA:
+		case FRUIT.BANANA:
 			System.out.println(93 + " kcal");
 			break;
 		}

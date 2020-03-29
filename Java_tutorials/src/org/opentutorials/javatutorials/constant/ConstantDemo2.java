50c65b9649b3d0dfb36542b0137b26d0a24a2b72
jinook929 <jinook929@gmail.com>
2020-03-28 21:39:43 -0400

    enum and for

--- Java_tutorials/src/org/opentutorials/javatutorials/constant/ConstantDemo2.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/constant/ConstantDemo2.java
@@ -1,7 +1,15 @@
 package org.opentutorials.javatutorials.constant;
 
 enum Fruit_enum {
-	APPLE, PEACH, BANANA;
+	APPLE("red"), PEACH("pink"), BANANA("yellow");
+	private String color; 
+	public String getColor() {
+		return this.color;
+	}
+	Fruit_enum(String color) {
+		System.out.println("Call constructor : " + this);
+		this.color = color;
+	}
 }
 
 enum Company_enum {
@@ -11,17 +19,21 @@ enum Company_enum {
 public class ConstantDemo2 {
 
 	public static void main(String[] args) {
+		
+		for(Fruit_enum f : Fruit_enum.values()) {
+			System.out.println(f);
+		}
 
 		Fruit_enum type__ = Fruit_enum.APPLE;
 		switch(type__) {
 			case APPLE:
-				System.out.println(57 + " kcal");
+				System.out.println(57 + " kcal, color = " + Fruit_enum.APPLE.getColor());
 				break;
 			case PEACH:
-				System.out.println(34 + " kcal");
+				System.out.println(34 + " kcal, color = " + Fruit_enum.PEACH.getColor());
 				break;
 			case BANANA:
-				System.out.println(93 + " kcal");
+				System.out.println(93 + " kcal, color = " + Fruit_enum.BANANA.getColor());
 				break;
 			}
 

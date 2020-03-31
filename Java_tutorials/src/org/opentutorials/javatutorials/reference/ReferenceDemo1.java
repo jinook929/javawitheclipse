b7ac7a527b853643bb316bdd53813e0975d5d52a
jinook929 <jinook929@gmail.com>
2020-03-30 21:31:59 -0400

    reference

--- Java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/reference/ReferenceDemo1.java
@@ -1,5 +1,12 @@
 package org.opentutorials.javatutorials.reference;
 
+class A {
+	public int id;
+	A(int id)  {
+		this.id = id;
+	}
+}
+
 public class ReferenceDemo1 {
 	
 	public static void runValue() {
@@ -8,10 +15,18 @@ public class ReferenceDemo1 {
 		b = 2;
 		System.out.println("runValue = " + a);
 	}
+	
+	public static void runReference() {
+		A a = new A(1);
+		A b = a;
+		b.id = 2;
+		System.out.println("runReference = " + a.id);
+	}
 
 	public static void main(String[] args) {
 		
 		runValue();
+		runReference();
 
 	}
 

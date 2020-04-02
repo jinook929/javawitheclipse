f265ecaf3552b6cded13b329dce4f4ff8edcde82
jinook929 <jinook929@gmail.com>
2020-04-01 21:27:05 -0400

    generic applied

--- Java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo1.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo1.java
@@ -5,29 +5,28 @@ class StudentInfo {
 	StudentInfo(int grade) { this.grade = grade; }
 }
 
-class StudentPerson {
-	public StudentInfo info;
-	StudentPerson(StudentInfo info) { this.info = info; }
-}
-
 class EmployeeInfo {
 	public int rank;
 	EmployeeInfo(int rank) { this.rank = rank; }
 }
 
-class EmployeePerson {
-	public EmployeeInfo info;
-	EmployeePerson(EmployeeInfo info) { this.info = info; }
+class Person_<T> {
+	public T info;
+	Person_(T info) { this.info = info; }
 }
 
 public class GenericDemo1 {
 
 	public static void main(String[] args) {
-		StudentInfo si = new StudentInfo(2);
-		StudentPerson sp = new StudentPerson(si);
-		System.out.println(sp.info.grade);
-		EmployeeInfo ei = new EmployeeInfo(1);
-		EmployeePerson ep = new EmployeePerson(ei);
-		System.out.println(ep.info.rank);
+		Person_<EmployeeInfo> p1 = new Person_<EmployeeInfo>(new EmployeeInfo(1));
+		EmployeeInfo ei = p1.info;
+		System.out.println(ei.rank);
+		
+//		StudentInfo si = new StudentInfo(2);
+//		Person sp = new Person(si);
+//		System.out.println(sp.info.grade);
+//		EmployeeInfo ei = new EmployeeInfo(1);
+//		EmployeePerson ep = new EmployeePerson(ei);
+//		System.out.println(ep.info.rank);
 	}
 }

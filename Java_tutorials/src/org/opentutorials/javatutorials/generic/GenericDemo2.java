d42125b5a238d26a4367eea83389cea6dfe9e06b
jinook929 <jinook929@gmail.com>
2020-04-02 20:28:14 -0400

    multiple generic

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo2.java
@@ -0,0 +1,29 @@
+package org.opentutorials.javatutorials.generic;
+
+class EmployeeInfoOne {
+	public int rank;
+	EmployeeInfoOne(int rank) {
+		this.rank = rank;
+	}
+}
+
+class PersonOne<T, S> {
+	public T info;
+	public S id;
+	PersonOne(T info, S id) {
+		this.info = info;
+		this.id = id;
+	}
+}
+
+public class GenericDemo2 {
+
+	public static void main(String[] args) {
+
+		PersonOne<EmployeeInfoOne, Integer> p1 = new PersonOne<EmployeeInfoOne, Integer>(new EmployeeInfoOne(3), 1);
+		System.out.println(p1.id);
+		System.out.println(p1.info.rank);
+
+	}
+
+}

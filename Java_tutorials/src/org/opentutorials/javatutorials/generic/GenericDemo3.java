c1b2f56bde4fffedcb5a7e5a8dd2912f3adf5f82
jinook929 <jinook929@gmail.com>
2020-04-02 20:56:14 -0400

    generic

---
+++ Java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo3.java
@@ -0,0 +1,32 @@
+package org.opentutorials.javatutorials.generic;
+
+interface Info {
+	int getLevel();
+}
+
+class EmployeeInfoTwo implements Info {
+	public int rank;
+	EmployeeInfoTwo(int rank) {
+		this.rank = rank;
+	}
+	public int getLevel() {
+		return this.rank;
+	}
+}
+
+class PersonTwo<T extends Info> {
+	public T info;
+	PersonTwo(T info) {
+		this.info = info;
+		info.getLevel();
+	}
+}
+
+public class GenericDemo3 {
+
+	public static void main(String[] args) {
+		PersonTwo<EmployeeInfoTwo> p1 = new PersonTwo<EmployeeInfoTwo>(new EmployeeInfoTwo(10));
+		System.out.println(p1.info.getLevel());
+	}
+
+}

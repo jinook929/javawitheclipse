72102b712662dc9e8dadff51ea3c74c7424404ae
jinook929 <jinook929@gmail.com>
2020-04-07 22:39:55 -0400

    Collection iterator

--- Java_tutorials/src/org/opentutorials/javatutorials/collection/SetDemo.java
+++ Java_tutorials/src/org/opentutorials/javatutorials/collection/SetDemo.java
@@ -1,5 +1,7 @@
 package org.opentutorials.javatutorials.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -15,6 +17,18 @@ public class SetDemo {
 		Iterator<Integer> hi = A.iterator();
 		while(hi.hasNext()) {
 			System.out.println(hi.next());
+		}
+		
+		ArrayList<Integer> B = new ArrayList<>();
+//		Collection<Integer> B = new ArrayList<>();
+//		Collection<Integer> B = new HashSet<>();
+		B.add(4);
+		B.add(5);
+		B.add(6);
+		
+		Iterator<Integer> hiB = B.iterator();
+		while(hiB.hasNext()) {
+			System.out.println(hiB.next());
 		}
 
 	}
